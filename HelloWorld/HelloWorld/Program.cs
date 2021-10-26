7958fbfda6bf729286b3dfc515206877eb2ffc5f
gbaltazar-personal <gryson@gmail.com>
2021-10-26 09:16:38 -0700

    helloworld source

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Gryson!");
+            Console.Read();  //read the next character in the standard input stream
+        }
+    }
+}
